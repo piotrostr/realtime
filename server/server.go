3de21e050d29a2ba67157b676f1f24f8c2e313ed
piotrostr <piotr.ostrowski@aol.com>
2022-09-04 17:24:54 +0200

    add todos for server

---
+++ server/server.go
@@ -0,0 +1,21 @@
+package server
+
+// endpoints for crud
+
+// there is an event for updates
+
+// websocket connections, sending the realtime data,
+// sending a new batch on
+// every update
+
+// in the frontend:
+// use a redux reducer to get all of the important parts of the data
+
+// generally:
+
+// read after successful handshake, send the data
+
+// allow updates from authenticated clients to be sent
+
+// every time there is a new entry
+// send the ?{new entry or the whole updated batch}?
